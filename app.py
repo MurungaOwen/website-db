82eaff3ee3879a182245a308cb4ff477efae7d51
MurungaOwen <122882250+MurungaOwen@users.noreply.github.com>
2023-04-30 16:06:41 +0000

    added a html doc to refer to and styled it

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')  #the homepage...the @  is like a decorator
 def home():
-  return "hood"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)#specify ythe host that is in replit
+  app.run(host='0.0.0.0', debug=True)  #specify ythe host that is in replit

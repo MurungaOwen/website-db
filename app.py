4d6aaf469df1260129f7b7f11960d423913a819e
MurungaOwen <122882250+MurungaOwen@users.noreply.github.com>
2023-05-02 17:13:03 +0000

    connector to database

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 from flask import Flask, render_template,jsonify
+from database import load_jobs,engine
 
 app = Flask(__name__)
-JOBS=[{
-    'id':'1',
-    'title':'software engineer',
-    'location':'kisumu',
-    'salary':243000,
-  },
-  {
-    'id':'2',
-    'title':'Backend engineer',
-    'location':'Nairobi',
-    'salary':430000,
-  },
-  {
-    'id':'3',
-    'title':'web designer',
-    'location':'Mombasa',
-    'salary':234000,
-  }]
+
 
 @app.route('/')  #the homepage...the @  is like a decorator
 def home():
-  return render_template('home.html',jobs=JOBS)
+  jobs = load_jobs()
+  return render_template('home.html', jobs=jobs)
 
-@app.route('/api/jobs')
-def jobs():
-  return jsonify(JOBS)
-  
+
+@app.route('/no')
+def listjobs():
+  jobs = load_jobs()
+  return jsonify(jobs)
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)  #specify ythe host that is in replit
